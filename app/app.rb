21d6a9f32f2f13e53d6f17c8cfa0fda2784eaf05
rcvink <rrvink@gmail.com>
2018-01-23 15:12:44 +0000

    Implement new link form

--- app/app.rb
+++ app/app.rb
@@ -5,11 +5,21 @@ class Bookmark_Manager < Sinatra::Base
 
   run! if app_file == $0
 
-  # get '/' do
-  #   redirect '/links'
-  # end
+  get '/' do
+    redirect '/links'
+  end
 
   get '/links' do
+    @links = Link.all
+    erb(:links)
+  end
+
+  get '/links/new' do
+    erb(:add_new_link)
+  end
+
+  post '/links' do
+    Link.create(url: params[:new_link_url], title: params[:new_link_title])
     @links = Link.all
     erb(:links)
   end
---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,10 @@
+feature 'adding link' do
+  scenario 'user can add a link' do
+    visit('/links')
+    click_button 'add link'
+    fill_in :new_link_title, with: 'Wikipedia'
+    fill_in :new_link_url, with: 'http://www.wikipedia.org'
+    click_button 'submit'
+    expect(page).to have_content('Wikipedia')
+  end
+end
