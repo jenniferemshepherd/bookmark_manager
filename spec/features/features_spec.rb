263b783b1465c14ef5d4b1f216ed37d645e0b9cf
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-23 11:31:26 +0000

    feature tests set up

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,17 @@
+feature 'homepage' do
+  scenario 'homepage exists' do
+    visit('/')
+    expect(page.status_code).to eq 200
+    expect(page).to have_content "Here are your bookmarks:"
+  end
+end
+
+feature 'Viewing Links' do
+  scenario 'I can existing link' do
+    Link.create(url: 'http://www.elliewem.co.uk', title: 'Ellie Wem')
+    visit('/links')
+    within 'ul#links' do
+      expect(page).to have_content('Ellie Wem')
+    end
+  end
+end
